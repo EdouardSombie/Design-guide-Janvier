0876df10331e1e7b60ed97ff1fb2c92d2c5224e2
Edouard <edouard@voltajazz.com>
2022-06-13 12:19:27 +0200

    Commit 2

--- dist/main.js
+++ dist/main.js
@@ -4040,6 +4040,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
     return C.$ === S && (C.$ = Gt), e && C.jQuery === S && (C.jQuery = Vt), S;
   }, "undefined" == typeof e && (C.jQuery = C.$ = S), S;
 });
+var intervals = {};
 
 function sliderInit(element) {
   var container = $('<div/>').addClass('slides-container');
@@ -4053,7 +4054,8 @@ function sliderInit(element) {
     prev(e.target);
   });
   var navigation = $('<nav/>').append(btnPrev).append(btnNext);
-  element.html(container).addClass('slider').append(navigation).attr('currentImage', '0');
+  element.html(container).addClass('slider').append(navigation).attr('currentImage', '0').attr('id', 'slider' + $('.slider').length);
+  autoPlay(element);
 }
 
 function next(btn) {
@@ -4061,6 +4063,7 @@ function next(btn) {
   var currentImage = slider.attr('currentImage') != undefined ? slider.attr('currentImage') : 0;
   slider.attr('currentImage', Number(currentImage) + 1);
   slide(slider);
+  autoPlay(slider);
 }
 
 function prev(btn) {
@@ -4068,11 +4071,94 @@ function prev(btn) {
   var currentImage = slider.attr('currentImage') != undefined ? slider.attr('currentImage') : 0;
   slider.attr('currentImage', Number(currentImage) - 1);
   slide(slider);
+  autoPlay(slider);
 }
 
 function slide(slider) {
+  disableNav(slider);
   var currentImage = slider.attr('currentImage') != undefined ? slider.attr('currentImage') : 0;
-  var container = slider.children('.slides-container');
+  var container = slider.children('.slides-container'); // Si on dépasse la dernière image
+
+  if (currentImage >= container.children('.slide').length) {
+    // créer un clone de la première image
+    var clone = container.children('.slide:first-child').clone(); // ajouter le clone au bandeau
+
+    container.append(clone); // écouter la fin de la transition
+
+    container.on('transitionend', function () {
+      // Supprimer l'ecouteur pour qu'il ne joue qu'une fois 
+      $(this).off('transitionend'); // enlever la transition sur le bandeau
+
+      container.css('transition', 'none'); // rembobiner le bandeau vers l'image 1
+
+      slider.attr('currentImage', 0);
+      slide(slider); // supprimer le clone
+
+      clone.remove(); // remettre la transition sur le bandeau après un timeout
+
+      setTimeout(function () {
+        container.css('transition', 'left 1s');
+      }, 100);
+    });
+  } // Si on va avant la première image
+
+
+  if (currentImage <= -1) {
+    // créer un clone de la dernière image
+    var clone = container.children('.slide:last-child').clone(); // ajouter le clone avant la première image
+
+    container.prepend(clone);
+    clone.css({
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      transform: 'translateX(-100%)'
+    }); // écouter la fin de la transition
+
+    container.on('transitionend', function () {
+      $(this).off('transitionend'); // enlever la transition
+
+      container.css('transition', 'none'); // rembobiner vers la dernière image
+
+      var lastIndex = container.children('.slide').length - 1 - 1;
+      slider.attr('currentImage', lastIndex);
+      slide(slider);
+      clone.remove(); // remettre la transition
+
+      setTimeout(function () {
+        container.css('transition', 'left 1s');
+      }, 10);
+    });
+  } // ajouter un ecouteur de fin de transition pour rétablir la nav
+
+
+  container.on('transitionend', function () {
+    container.off('transitionend');
+    enableNav(slider);
+  });
   var leftValue = currentImage * -100 + '%';
   container.css('left', leftValue);
+}
+
+function enableNav(slider) {
+  slider.find('button').removeAttr('disabled');
+}
+
+function disableNav(slider) {
+  slider.find('button').attr('disabled', 'disabled');
+}
+
+function autoPlay(slider) {
+  var idSlider = slider.attr('id'); // Suppression de l'intervalle s'il existe
+
+  if (undefined != intervals[idSlider]) {
+    var _interval = intervals[idSlider];
+    clearInterval(_interval);
+  } // Créer un intervalle pour le slider
+
+
+  var interval = setInterval(function () {
+    next(slider);
+  }, 4000);
+  intervals[idSlider] = interval;
 }
--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,3 +1,5 @@
+var intervals = {};
+
 function sliderInit(element){
 	var container = $('<div/>')
 	.addClass('slides-container');
@@ -22,7 +24,10 @@ function sliderInit(element){
 	.html(container)
 	.addClass('slider')
 	.append(navigation)
-	.attr('currentImage', '0');
+	.attr('currentImage', '0')
+	.attr('id','slider' + $('.slider').length);
+
+	autoPlay(element);
 }
 
 function next(btn){
@@ -30,6 +35,7 @@ function next(btn){
 	var currentImage = slider.attr('currentImage') != undefined ? slider.attr('currentImage') : 0;
 	slider.attr('currentImage', Number(currentImage) + 1);
 	slide(slider);
+	autoPlay(slider)
 }
 
 
@@ -38,16 +44,116 @@ function prev(btn){
 	var currentImage = slider.attr('currentImage') != undefined ? slider.attr('currentImage') : 0;
 	slider.attr('currentImage', Number(currentImage) - 1);
 	slide(slider);
+	autoPlay(slider)
 }
 
 function slide(slider){
+
+	disableNav(slider);
+
 	var currentImage = slider.attr('currentImage') != undefined ? slider.attr('currentImage') : 0;
 	var container = slider.children('.slides-container');
+	
+	// Si on dépasse la dernière image
+	if(currentImage >= container.children('.slide').length){
+		// créer un clone de la première image
+		var clone = container.children('.slide:first-child').clone();
+		// ajouter le clone au bandeau
+		container.append(clone);
+		// écouter la fin de la transition
+		container.on('transitionend', function(){
+
+			// Supprimer l'ecouteur pour qu'il ne joue qu'une fois 
+			$(this).off('transitionend');
+
+			// enlever la transition sur le bandeau
+			container.css('transition', 'none');
+
+			// rembobiner le bandeau vers l'image 1
+			slider.attr('currentImage', 0);
+			slide(slider);
+
+			// supprimer le clone
+			clone.remove();
+
+			// remettre la transition sur le bandeau après un timeout
+			setTimeout(function(){
+				container.css('transition', 'left 1s');
+			}, 100);
+			
+		})
+	}
+
+	// Si on va avant la première image
+	if(currentImage <= -1){
+		// créer un clone de la dernière image
+		var clone = container.children('.slide:last-child').clone();
+
+		// ajouter le clone avant la première image
+		container.prepend(clone);
+		clone.css({
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			transform: 'translateX(-100%)'
+		});
+
+		// écouter la fin de la transition
+		container.on('transitionend', function(){
+			$(this).off('transitionend');
+
+			// enlever la transition
+			container.css('transition', 'none');
+
+			// rembobiner vers la dernière image
+			let lastIndex = container.children('.slide').length - 1 -1
+			slider.attr('currentImage', lastIndex);
+			slide(slider);
+			clone.remove();
+
+			// remettre la transition
+			setTimeout(function(){
+				container.css('transition', 'left 1s');
+			},10);
+		})
+	}
+
+	// ajouter un ecouteur de fin de transition pour rétablir la nav
+	container.on('transitionend',function(){
+		container.off('transitionend');
+		enableNav(slider);
+	});
+
 	var leftValue = currentImage * (-100) + '%';
 	container.css('left', leftValue);
 }
 
 
+function enableNav(slider){
+	slider.find('button').removeAttr('disabled');
+}
+
+
+function disableNav(slider){
+	slider.find('button').attr('disabled','disabled');
+}
+
+function autoPlay(slider){
+	const idSlider = slider.attr('id');
+
+	// Suppression de l'intervalle s'il existe
+	if(undefined != intervals[idSlider]){
+		let interval = intervals[idSlider];
+		clearInterval(interval);
+	}
+
+	// Créer un intervalle pour le slider
+	var interval = setInterval(function(){
+		next(slider);
+	}, 4000);
+
+	intervals[idSlider] = interval;
+}
 
 
 
